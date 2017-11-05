334ef5ea575a8babdcdeef3cc1a6fbb4184df311
fawwadhussaini <fawwad.hussaini@gmail.com>
2017-11-05 10:39:54 +0500

    First 3 programs!

--- assignment_1.py
+++ assignment_1.py
@@ -4,7 +4,6 @@ def donuts(count):
     else:
         print('Number of donuts: many')
 
-
 donuts(9)
 
 
@@ -12,8 +11,12 @@ def both_ends(s):
     if (len(s) < 2):
         print(s)
     else:
-        print (s[0:2] + s[-2:])
+        print (s[0:2] + s[-2:8])
         print (s[0:2] + s[len(s)-2:])
 
-
 both_ends ('pakistan')
+
+def fix_start (f):
+    print (f[0:1] + f[1:].replace('c','^'))
+
+fix_start ('cricket by chance')
